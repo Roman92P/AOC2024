c5a7b81c22866a8dae5f81bdb24042a7bfe028aa
Roman Pashkov <roman.pashkov@capgemini.com>
2024-12-20 15:30:00 +0100

    work on Day2_2

--- Day2_2.py
+++ Day2_2.py
@@ -7,12 +7,12 @@ safe_count = 0
 
 
 def isSafe(set_diffs):
+    print('Checking diff', set_diffs)
     result = False
-    if (1 <= len(set_diffs) <= 3 and
-            1 <= min(abs(x) for x in set_diffs) <= 3 and
-            max(abs(x) for x in set_diffs) <= 3 and
-            (all(x < 0 for x in set_diffs) or all(x > 0 for x in set_diffs))):
-        result = True
+    if -3 <= min(abs(x) for x in set_diffs) <= 3 and\
+            max(abs(x) for x in set_diffs) <= 3 and\
+            (all(x < 0 for x in set_diffs) or all(x > 0 for x in set_diffs)):
+        return True
     return result
 
 
@@ -43,6 +43,7 @@ def get_corrupted_level_index(differences):
         else:
             return 0
 
+
 for i, l in enumerate(lines):
     a = l.strip()
     tmp_arr = list(map(int, a.split()))
@@ -53,20 +54,31 @@ for i, l in enumerate(lines):
         safe_count = safe_count + 1
         print('Safe', tmp_arr)
     else:
-        t_tmp_arr = tmp_arr.copy()
+        t_tmp_arr_1 = tmp_arr.copy()
+        t_tmp_arr_2 = tmp_arr.copy()
+        t_tmp_arr_3 = tmp_arr.copy()
         index = get_corrupted_level_index(differences)
         print('Wrong lv index: ', index)
         differences_list = list(differences)
-        t_tmp_arr.pop(index)
-        new_differences = np.diff(t_tmp_arr)
+        t_tmp_arr_1.pop(index)
+        new_differences = np.diff(t_tmp_arr_1)
         try:
-            tmp_arr.pop(index+1)
+            t_tmp_arr_2.pop(index - 1)
         except IndexError:
-            tmp_arr.pop(len(tmp_arr)-1)
-        new_differences_two = np.diff(tmp_arr)
+            tmp_arr.pop(index)
+        new_differences_two = np.diff(t_tmp_arr_2)
+        try:
+            t_tmp_arr_3.pop(index + 1)
+        except IndexError:
+            tmp_arr.pop(index)
+        new_differences_three = np.diff(t_tmp_arr_3)
+        print('New diffs', new_differences)
+        print('New diffs two', new_differences_two)
+        print('New diffs three', new_differences_three)
         set_diffs = set(new_differences)
         set_diffs_two = set(new_differences_two)
-        if isSafe(set_diffs) or isSafe(new_differences_two):
+        set_diffs_two = set(new_differences_three)
+        if isSafe(set_diffs) or isSafe(new_differences_two) or isSafe(new_differences_three):
             safe_count = safe_count + 1
             print('Safe', tmp_arr)
         else:
